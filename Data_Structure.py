3359a5cc08c03d8c274428e61bf7b6c5159ee836
Ketan <geedketan@gmail.com>
2026-08-19 21:58:16 +0530

    List Done

---
+++ Data_Structure.py
@@ -0,0 +1,31 @@
+# Task:
+
+# Create a list named colors containing three strings: "red", "green", and "blue".
+
+# Add "yellow" to the end of the list using .append().
+
+# Print the first item of the list using index 0.
+
+colors = ["red", "green", "blue"]
+colors.append("yellow")
+print(colors[0])
+
+
+
+
+# change the item...
+
+fruits = ["apple", "banana", "cherry"]
+
+fruits[1] = "mango"
+fruits.insert(0, "orange")
+print(fruits)
+
+
+
+numbers = [10, 20, 30, 40, 50]
+
+numbers.remove(30)
+numbers.pop(3)
+print(numbers)
+print(len(numbers))
