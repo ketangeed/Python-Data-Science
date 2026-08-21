b74dafbd2744a6ed21a4d431af01c8282c599832
Ketan <geedketan@gmail.com>
2026-08-21 19:33:38 +0530

    String DSA que

--- strings.py
+++ strings.py
@@ -13,22 +13,85 @@
 # Special: 2
 
 
-string = input("Enter the String : ")
-Uppercase =  0
-Lowercase = 0
-Digits = 0
-Special = 0
+# string = input("Enter the String : ")
+# Uppercase =  0
+# Lowercase = 0
+# Digits = 0
+# Special = 0
 
-for i in string:
-    if i.isupper():
-        Uppercase += 1
-    elif i.islower():
-        Lowercase += 1
-    elif i.isdigit():
-        Digits += 1
-    else :
-        Special += 1
-print(Uppercase)
-print(Lowercase)
-print(Digits)
-print(Special)
+# for i in string:
+#     if i.isupper():
+#         Uppercase += 1
+#     elif i.islower():
+#         Lowercase += 1
+#     elif i.isdigit():
+#         Digits += 1
+#     else :
+#         Special += 1
+# print(Uppercase)
+# print(Lowercase)
+# print(Digits)
+# print(Special)
+
+
+
+
+# Given a string, find the first character that appears only once.
+# Example 1
+# Input:
+# swiss
+# Output:
+# w
+
+# string_2 = input("Enter the String : ")
+
+# freq = {}
+
+# # Step 1: Count frequency of each character
+# for i in string_2:
+#     if i in freq:
+#         freq[i] += 1
+#     else:
+#         freq[i] = 1
+
+# # Step 2: Find the first unique character
+# found = False
+
+# for i in string_2:
+#     if freq[i] == 1:
+#         print(i)
+#         found = True
+#         break
+
+# # Step 3: If no unique character was found
+# if not found:
+#     print("No Unique Character")
+
+
+# string_3 = input("Enter the String : ")
+# commit = ""
+# for i in string_3:
+#     if i not in commit:
+#         commit += i
+
+#     else :
+#         continue
+# print(commit)
+
+
+
+# Write a program that takes a string and finds the character that occurs the maximum number of times.
+# Input:
+# programming
+# Output:
+# r → 2
+
+
+n = "banana"
+freq = {}
+for i in n:
+    if i in freq:
+        freq[i] += 1
+    else:
+        freq[i] = 1
+print(max(freq))
