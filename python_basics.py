0e2601b11b55cd68b49f488383bb27fcb4724212
Ketan <geedketan@gmail.com>
2026-08-24 22:02:04 +0530

    python refresher 4

--- python_basics.py
+++ python_basics.py
@@ -52,14 +52,107 @@
 # Handle uppercase letters too
 # Return the number of vowels
 
+# def count_vowels(name):
+#     vowels = "aeiou"
+#     count = 0
 
-def count_vowels(name):
-    vowels = "aeiou"
-    count = 0
-
-    for i in name.lower():
-        if i in vowels:
-            count += 1
-    return count
+#     for i in name.lower():
+#         if i in vowels:
+#             count += 1
+#     return count
     
-print(count_vowels("Hello World"))
+# print(count_vowels("Hello World"))
+
+
+
+# Imagine you receive names from a dataset:
+# "  rahul "
+# "RAHUL"
+# " Rahul"
+# "rahul  "
+# Your job is to create a function:
+# clean_name(name)
+# that:
+# Removes unnecessary spaces from the beginning/end.
+# Converts the name to lowercase.
+# Returns the cleaned name.
+
+# def clean_name(name):
+#     return name.lower().strip()
+# b = clean_name(" KETAN ")
+# print(b)
+
+
+
+# names = ["  Rahul ", "KETAN", " rahul", "Amit ", "Ketan"]
+
+# for i in names:
+#    print(clean_name(i))
+
+
+
+
+
+# Create:
+# def check_email(email):
+# It should return:
+# Valid
+# if the email contains:
+# @
+# .
+# and @ comes before .
+# Otherwise return:
+# Invalid
+
+
+# def check_email(email):
+#     at_position = email.find("@")
+#     dot_position = email.find(".")
+#     if at_position < dot_position and at_position != -1 and dot_position != -1:
+#         return "Valid.."
+
+#     else:
+#         return "Invalid..."
+
+
+# mail = check_email("keta@gmail.com")
+# print(mail)
+
+
+
+
+
+
+# names = ["  Rahul", "KETAN ", " amit ", "ROHAN", "  Priya  "]
+
+
+
+# def cleaned_names(list):
+#     cleaned = []
+#     for i in list:
+        
+#         a = i.lower().strip()
+#         cleaned.append(a)
+#     return cleaned
+
+# data = cleaned_names(names)
+# print(data)
+
+
+
+
+# filter even
+
+numbers = [12, 5, 8, 21, 30, 7, 16, 3]
+
+def filter_even(list1):
+    even_num = []
+    for num in list1:
+        if num % 2 == 0:
+            even_num.append(num)
+        else:
+            continue
+    return even_num
+
+n = filter_even(numbers)
+print(n)
